9386aa9d2a67562b97bf6a37dc82fb79b5012569
Steven Meyers <sjjmeyers@gmail.com>
2020-01-30 15:08:21 -0700

    Add methods to Event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,8 +1,27 @@
 class Event
-def initialize(event_name)
-  @argument = argument
-end
+  attr_reader :name, :ages
 
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+  end
 
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+
+  def average_age
+    average_age = ages.sum.to_f / ages.count
+    average_age.round(2)
+  end
+
+  def standard_deviation_age
+    
+
+  end
 
 end
