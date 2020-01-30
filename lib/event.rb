71ed7ba08d1fef411151d8229ba203139e9d8d6f
Steven Meyers <sjjmeyers@gmail.com>
2020-01-30 16:07:50 -0700

    Edit Event class

--- lib/event.rb
+++ lib/event.rb
@@ -15,12 +15,28 @@ class Event
   end
 
   def average_age
-    average_age = ages.sum.to_f / ages.count
-    average_age.round(2)
+    @average_age = ages.sum.to_f / ages.count
+    @average_age.round(2)
   end
 
   def standard_deviation_age
-    
+    @ages_mean = average_age
+    ages_step_4 = []
+
+    ages.each do |age|
+      ages_step_4 << age - @ages_mean
+    end
+    ages_step_5 = []
+    ages_step_4.each do |age|
+      ages_step_5 << age * age
+    end
+
+    ages_step_6 = ages_step_5.sum
+
+    ages_step_7 = ages_step_6 / ages.count
+
+    standard_deviation = Math.sqrt(ages_step_7).round(2)
+
 
   end
 
